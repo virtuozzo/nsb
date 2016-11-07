5f741e76412136c258dddf414576e45245285e9c
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-11-07 15:58:08 +0300

    patcher: isolate options parsing
    And fix an segfault issue when command is not provided.
    And check that pid is provided.
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- patcher/main.c
+++ patcher/main.c
@@ -3,55 +3,73 @@
 #include <string.h>
 #include <getopt.h>
 
-#include "version.h"
-#include "patch.h"
-#include "log.h"
+#include "include/version.h"
+#include "include/patch.h"
+#include "include/log.h"
 
 /* Stub for compel */
 int compel_main(void *arg_p, unsigned int arg_s) { return 0; }
 
-int main(int argc, char *argv[])
-{
-	pid_t pid = 0;
-	int opt, idx;
+struct options {
+	pid_t		 pid;
+	const char	*patch_path;
+	int		verbosity;
+	char		*command;
+};
 
-	static const char short_opts[] = "p:v:";
+static int parse_options(int argc, char **argv, struct options *o)
+{
+
+	static const char short_opts[] = "p:v:f:";
 	static struct option long_opts[] = {
 		{ "pid",			required_argument,	0, 'p'	},
 		{ "log-level",			required_argument,	0, 'v'	},
+		{ "filename",			required_argument,	0, 'f'	},
 		{ },
 	};
-
-	if (argc < 2)
-		goto usage;
+	int opt, idx = -1;
 
 	while (1) {
-		idx = -1;
 		opt = getopt_long(argc, argv, short_opts, long_opts, &idx);
 		if (opt == -1)
 			break;
 
 		switch (opt) {
 		case 'p':
-			pid = atoi(optarg);
-			if (pid <= 0)
+			o->pid = atoi(optarg);
+			if (o->pid <= 0)
 				goto bad_arg;
 			break;
 		case 'v':
-			log_set_loglevel(atoi(optarg));
+			o->verbosity = atoi(optarg);
+			if (o->verbosity <= 0)
+				goto bad_arg;
+			break;
+		case 'f':
+			o->patch_path = optarg;
 			break;
 		default:
 			goto usage;
-			break;
 		}
 	}
 
-	log_init(NULL);
+	if (!o->pid) {
+		pr_msg("Error: process pid has to be provided\n");
+		goto usage;
+	}
 
-	if (!strcmp(argv[optind], "patch"))
-		return patch_process(pid, 4096);
+	if (optind == argc) {
+		pr_msg("Error: command has to be provided\n");
+		goto usage;
+	}
+	if (argc > optind + 1) {
+		pr_msg("Error: only one command has to be provided\n");
+		goto usage;
+	}
 
-	pr_msg("Error: unknown command: %s\n", argv[optind]);
+	o->command = argv[optind];
+	return 0;
+
 usage:
 	pr_msg("\n"
 "Usage:\n"
@@ -66,5 +84,26 @@ bad_arg:
 	else
 		pr_msg("Error: invalid argument for --%s: %s\n",
 		       long_opts[idx].name, optarg);
+
 	return 1;
+}
+
+int main(int argc, char *argv[])
+{
+	struct options o = {};
+	int err;
+
+	err = parse_options(argc, argv, &o);
+	if (err)
+		return err;
+
+	log_set_loglevel(o.verbosity);
+	log_init(NULL);
+
+	if (strcmp(o.command, "patch")) {
+		pr_msg("Error: unknown command: %s\n", argv[optind]);
+		return -1;
+	}
+
+	return patch_process(o.pid, 4096);
 }
