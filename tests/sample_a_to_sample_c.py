6717b0da90be3f5fbf4b629985f25869bab79cf1
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-11-10 18:15:54 +0300

    sources: add regression tests
    Only three so far:
       sample_a_to_sample_b.py
       sample_a_to_sample_c.py
       sample_a_to_sample_cc.py
    
    Would be very nice to add tests generator
    
    Can be run with "make check"
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

---
+++ tests/sample_a_to_sample_c.py
@@ -0,0 +1,4 @@
+import testrunner
+
+lpt = testrunner.LivePatchTest("./sample_a", "./sample_c")
+lpt.run()
---
+++ tests/sample_a_to_sample_f.py
@@ -0,0 +1,4 @@
+import testrunner
+
+lpt = testrunner.LivePatchTest("./sample_a", "./sample_f")
+lpt.run()
