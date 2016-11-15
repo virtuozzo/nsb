396b296a0353a1b54c361cd01d8ffe4bfa91db09
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-11-15 20:37:33 +0300

    patcher: helper for address offset generation added
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- patcher/x86_64.c
+++ patcher/x86_64.c
@@ -8,20 +8,48 @@
 #define OP_JMPQ		0xe9
 #define OP_JMP		0xeb
 
-int ip_change_relative(unsigned char *buf, unsigned char opcode,
+static long ip_gen_offset(unsigned long cur_pos, unsigned long tgt_pos,
+			  char addr_size)
+{
+	int i;
+	unsigned long offset;
+	unsigned long mask = 0;
+
+	for (i = 0; i < addr_size; i++) {
+		mask |= ((unsigned long)0xff << (8 * i));
+	}
+
+	offset = tgt_pos - cur_pos;
+	if (offset & ~mask) {
+		pr_err("%s: offset is beyond command size: %#lx > %#lx\n",
+				__func__, offset, mask);
+		return -EINVAL;
+	}
+
+	pr_debug("%s: addr_size : %d\n", __func__, addr_size);
+	pr_debug("%s: cur_pos : %#lx\n", __func__, cur_pos);
+	pr_debug("%s: tgt_pos : %#lx\n", __func__, tgt_pos);
+	pr_debug("%s: offset  : %#lx\n", __func__, offset);
+
+	return offset;
+}
+
+static int ip_change_relative(unsigned char *buf, unsigned char opcode,
 			      unsigned long cur_pos, unsigned long tgt_pos,
 			      size_t cmd_size)
 {
 	unsigned char *instr = &buf[0];
 	unsigned int *addr = (unsigned int *)&buf[1];
+	long offset;
 	int i;
 
-	*instr = opcode;
-	*addr = tgt_pos - cur_pos - cmd_size;
+	offset = ip_gen_offset(cur_pos + cmd_size, tgt_pos, cmd_size - 1);
+	if (offset < 0)
+		return offset;
 
-	pr_debug("%s: cur_pos : %#lx\n", __func__, cur_pos);
-	pr_debug("%s: tgt_pos : %#lx\n", __func__, tgt_pos);
-	pr_debug("%s: offset  : %#lx\n", __func__, tgt_pos - cur_pos - cmd_size);
+	*instr = opcode;
+	*addr = offset;
+
 	pr_debug("%s: bytes   :", __func__);
 	for (i = 0; i < cmd_size; i++)
 		pr_msg(" %02x", buf[i]);
