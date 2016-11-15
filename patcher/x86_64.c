69ed38cf5b46ab701ed8e9657598610e0dda14d2
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-11-15 18:42:23 +0300

    patcher: pass command size to ip_change_relative
    Not any Ip relative command is size of 5 bytes.
    Say, "jmp" is 2 bytes only.
    So, prepare for this.
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- patcher/x86_64.c
+++ patcher/x86_64.c
@@ -6,21 +6,21 @@
 #define X64_JMPQ	0xe9
 
 static int ip_change_relative(unsigned char *buf, unsigned char opcode,
-			      unsigned long cur_pos, unsigned long tgt_pos)
+			      unsigned long cur_pos, unsigned long tgt_pos,
+			      size_t cmd_size)
 {
 	unsigned char *instr = &buf[0];
 	unsigned int *addr = (unsigned int *)&buf[1];
 	int i;
-	size_t cmd_size = 5;
 
 	*instr = opcode;
-	*addr = tgt_pos - cur_pos - 5;
+	*addr = tgt_pos - cur_pos - cmd_size;
 
 	pr_debug("%s: cur_pos : %#lx\n", __func__, cur_pos);
 	pr_debug("%s: tgt_pos : %#lx\n", __func__, tgt_pos);
 	pr_debug("%s: offset  : %#lx\n", __func__, tgt_pos - cur_pos - cmd_size);
 	pr_debug("%s: bytes   :", __func__);
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < cmd_size; i++)
 		pr_msg(" %02x", buf[i]);
 	pr_debug("\n");
 
@@ -32,9 +32,9 @@ int x86_create_instruction(unsigned char *buf, int type,
 {
 	switch (type) {
 		case OBJ_INFO__OBJ_TYPE__CALL:
-			return ip_change_relative(buf, X64_CALLQ, cur_pos, tgt_pos);
+			return ip_change_relative(buf, X64_CALLQ, cur_pos, tgt_pos, 5);
 		case OBJ_INFO__OBJ_TYPE__JMPQ:
-			return ip_change_relative(buf, X64_JMPQ, cur_pos, tgt_pos);
+			return ip_change_relative(buf, X64_JMPQ, cur_pos, tgt_pos, 5);
 	}
 	pr_debug("%s: unknown object type: %d\n", __func__, type);
 	return -1;
