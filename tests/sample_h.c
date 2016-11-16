abc9e0022995a619ad47e22673e56736f6e8cfab
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-11-16 16:24:48 +0300

    tests: test mov (32 bit) command support
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

---
+++ tests/sample_h.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+#include "waitsig.h"
+
+static int var;
+
+int __attribute__ ((noinline)) func_b(void)
+{
+	var += 2;
+	return var + 3;
+}
+
+int __attribute__ ((noinline)) caller(void)
+{
+	return func_b();
+}
+
+int main(int argc, char **argv)
+{
+	return call_after_sig(caller);
+}
