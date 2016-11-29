8bf20e70e25234fd6fc7eac7d114d8c1381cf792
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-11-29 18:23:16 +0300

    Makefile.am: get rid of files copying
    Export PYTHONPATH in generated sources instead
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- tests/testgen.py
+++ tests/testgen.py
@@ -9,8 +9,11 @@ args = parser.parse_args()
 if args.outfile is None:
 	args.outfile = "%s_to_%s.py" % (args.source, args.target)
 
-code =	"import testrunner\n\n" +	\
-	"exit(testrunner.LivePatchTest('%s', '%s').run())\n" % (args.source, args.target)
+code = "import os\n" +								\
+	"import testrunner\n\n" +						\
+	"os.environ['PYTHONPATH'] = os.getcwd() + \"/protobuf\"\n" +            \
+	"exit(testrunner.LivePatchTest('%s', '%s').run())\n" %			\
+	(args.source, args.target)
 
 f = os.open(args.outfile, os.O_WRONLY | os.O_CREAT)
 os.write(f, code)
