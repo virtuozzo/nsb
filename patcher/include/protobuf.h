92005bacaf484c6c9357b710fa1dcd76ed5c56b7
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-12-09 18:03:44 +0300

    Makefile.am: change include path from protobuf to top_src
    This allows to compile sources, depending on protobuf headers, one by one like
    this:
    
    make patcher/patch.o
    
    from top directory.
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- patcher/include/protobuf.h
+++ patcher/include/protobuf.h
@@ -1,9 +1,9 @@
 #ifndef __PATCHER_PROTOBUF_H__
 #define __PATCHER_PROTOBUF_H__
 
-#include "funcpatch.pb-c.h"
-#include "binpatch.pb-c.h"
-#include "objinfo.pb-c.h"
+#include <protobuf/funcpatch.pb-c.h>
+#include <protobuf/binpatch.pb-c.h>
+#include <protobuf/objinfo.pb-c.h>
 
 FuncPatch *read_funcpatch(const char *path);
 BinPatch *read_binpatch(const char *path);
