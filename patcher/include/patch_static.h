feca99cd2f493671c068e4842ffc38f0dac9fc39
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-02-21 14:21:37 +0300

    patcher: move "static code"-related patching code to a separated file
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

---
+++ patcher/include/patch_static.h
@@ -0,0 +1,6 @@
+#ifndef __PATCHER_PATCH_STATIC_H__
+#define __PATCHER_PATCH_STATIC_H__
+
+int apply_exec_binpatch(struct process_ctx_s *ctx);
+
+#endif /* __PATCHER_PATCH_STATIC_H__ */
---
+++ patcher/patch_static.c
@@ -0,0 +1,150 @@
+#include "include/log.h"
+#include "include/xmalloc.h"
+#include "include/list.h"
+
+#include "include/process.h"
+#include "include/x86_64.h"
+
+static const struct funcpatch_s *search_func_by_name(const struct binpatch_s *bp, const char *name)
+{
+	const struct funcpatch_s *funcpatch;
+
+	list_for_each_entry(funcpatch, &bp->functions, list) {
+		if (!strcmp(funcpatch->fp->name, name))
+			return funcpatch;
+	}
+	return NULL;
+}
+
+static int apply_objinfo(struct process_ctx_s *ctx, unsigned long start, ObjInfo *oi)
+{
+	unsigned char code[X86_MAX_SIZE];
+	ssize_t size;
+	int err, i;
+	unsigned long where = start + oi->offset;
+
+	pr_debug("\t\tinfo: name     : %s\n", oi->name);
+	pr_debug("\t\tinfo: op_size  : %#x\n", oi->op_size);
+	pr_debug("\t\tinfo: addr_size: %#x\n", oi->addr_size);
+	pr_debug("\t\tinfo: offset   : %#x\n", oi->offset);
+	pr_debug("\t\tinfo: ref_addr : %#x\n", oi->ref_addr);
+
+	if (oi->ref_addr == 0) {
+		const struct funcpatch_s *funcpatch;
+
+		/* This means, that function is a new one */
+		funcpatch = search_func_by_name(&ctx->binpatch, oi->name);
+		if (!funcpatch) {
+			pr_err("\t\tfailed to find function by name %s\n", oi->name);
+			return -EINVAL;
+		}
+		pr_debug("\t\tfunction address : %#lx\n", funcpatch->addr);
+		oi->ref_addr = funcpatch->addr;
+	}
+
+	err = process_read_data(ctx->pid, where, code, round_up(sizeof(code), 8));
+	if (err < 0) {
+		pr_err("failed to read process address %ld: %d\n", where, err);
+		return err;
+	}
+
+	pr_debug("\t\tinfo: old code :");
+	for (i = 0; i < X86_MAX_SIZE; i++)
+		pr_msg(" %02x", code[i]);
+	pr_debug("\n");
+
+	size = x86_modify_instruction(code, oi->op_size, oi->addr_size,
+			where, oi->ref_addr);
+	if (size < 0)
+		return size;
+
+	pr_debug("\t\tinfo: new code :");
+	for (i = 0; i < X86_MAX_SIZE; i++)
+		pr_msg(" %02x", code[i]);
+	pr_debug("\n");
+
+	err = process_write_data(ctx->pid, where, code, round_up(sizeof(code), 8));
+	if (err < 0) {
+		pr_err("failed to write process address %ld: %d\n", where, err);
+		return err;
+	}
+	return 0;
+}
+
+static int apply_funcpatch(struct process_ctx_s *ctx, unsigned long addr, FuncPatch *fp)
+{
+	int i, err = 0;
+	unsigned char jump[X86_MAX_SIZE];
+	ssize_t size;
+
+	pr_debug("\tpatch: name : %s\n", fp->name);
+	pr_debug("\tpatch: addr : %#lx\n", fp->addr);
+	pr_debug("\tpatch: size : %d\n", fp->size);
+	pr_debug("\tpatch: new  : %d\n", fp->new_);
+	pr_debug("\tpatch: code :");
+	for (i = 0; i < fp->size; i++)
+		pr_msg(" %02x", fp->code.data[i]);
+	pr_debug("\n");
+	pr_debug("\tplace address  : %#lx\n", addr);
+
+	err = process_write_data(ctx->pid, addr, fp->code.data,
+			round_up(fp->size, 8));
+	if (err < 0) {
+		pr_err("failed to patch: %d\n", err);
+		return err;
+	}
+
+	for (i = 0; i < fp->n_objs; i++) {
+		pr_debug("\tObject info %d:\n", i);
+		err = apply_objinfo(ctx, addr, fp->objs[i]);
+		if (err)
+			return err;
+	}
+
+	if (!fp->new_) {
+		pr_debug("\tredirect to %#lx (overwrite %#lx)\n", addr, fp->addr);
+		size = x86_jmpq_instruction(jump, fp->addr, addr);
+		if (size < 0)
+			return size;
+
+		err = process_write_data(ctx->pid, fp->addr, jump, round_up(size, 8));
+		if (err < 0)
+			pr_err("failed to patch: %d\n", err);
+	}
+	return err;
+}
+
+int apply_exec_binpatch(struct process_ctx_s *ctx)
+{
+	int i, err = 0;
+	struct binpatch_s *binpatch = &ctx->binpatch;
+	BinPatch *bp = binpatch->bp;
+	struct funcpatch_s *funcpatch;
+
+	for (i = 0; i < bp->n_patches; i++) {
+		FuncPatch *fp = bp->patches[i];
+		unsigned long addr;
+
+		addr = process_get_place(ctx, fp->addr, fp->size);
+		if (addr < 0)
+			return addr;
+
+		funcpatch = xmalloc(sizeof(*funcpatch));
+		if (!funcpatch)
+			return -ENOMEM;
+
+		funcpatch->addr = addr;
+		funcpatch->fp = fp;
+		list_add_tail(&funcpatch->list, &binpatch->functions);
+	}
+
+	list_for_each_entry(funcpatch, &binpatch->functions, list) {
+		pr_debug("Function patch \"%s\"\n", funcpatch->fp->name);
+
+		err = apply_funcpatch(ctx, funcpatch->addr, funcpatch->fp);
+		if (err)
+			break;
+	}
+
+	return err;
+}
