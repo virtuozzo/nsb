245eeaedac50dc8432f3bb4709c7a53b6dd27a01
Eugene Kolomeetz <klm@virtuozzo.com>
2017-05-11 19:07:06 +0300

    generator: expand memoize() implementation
    Different dict classes may be used for each function argument.
    
    Signed-off-by: Eugene Kolomeetz <klm@virtuozzo.com>

--- generator/debuginfo.py
+++ generator/debuginfo.py
@@ -91,16 +91,33 @@ def get_die_size(die):
 	else:
 		assert 0
 
-def memoize(dict_class):
+def memoize(*dict_classes):
+	first_class  = dict_classes[0]
+	rest_classes = dict_classes[1:]
+
 	def fix_dict_class(f):
-		cache = dict_class()
+		cache = first_class()
 
 		@functools.wraps(f)
-		def wrapper(arg):
-			res = cache.get(arg)
-			if res is not None:
+		def wrapper(*args):
+			assert len(args) == len(dict_classes)
+
+			res = cache
+			for x in args:
+				res = res.get(x)
+				if res is None:
+					break
+			else:
 				return res
-			res = cache[arg] = f(arg)
+
+			c = cache
+			for x, dc in zip(args, rest_classes):
+				cn = c.get(x)
+				if cn is None:
+					cn = c[x] = dc()
+				c = cn
+
+			res = c[args[-1]] = f(*args)
 			return res
 
 		return wrapper
