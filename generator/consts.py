b459b25633be11c6842a236b036cbc0f6941d7bc
Eugene Kolomeetz <klm@virtuozzo.com>
2017-06-07 17:27:34 +0300

    generator: allow filtering by prefix in set_const_{raw,str}()
    Signed-off-by: Eugene Kolomeetz <klm@virtuozzo.com>

--- generator/consts.py
+++ generator/consts.py
@@ -4,15 +4,19 @@ class RAW(object):
 class STR(object):
 	pass
 
-def set_const_raw(const_dict):
+def set_const_raw(const_dict, prefix=None):
 	for name, value in const_dict.iteritems():
 		if name.startswith('_'):
 			continue
+		if prefix and not name.startswith(prefix):
+			continue
 		setattr(RAW, name, value)
 
-def set_const_str(const_dict):
+def set_const_str(const_dict, prefix=None):
 	for name in const_dict:
 		if name.startswith('_'):
+			continue
+		if prefix and not name.startswith(prefix):
 			continue
 		setattr(STR, name, name)
 
