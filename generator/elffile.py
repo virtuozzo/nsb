74802f7e7991ba10dfe1a9b084a2c2431fee83d6
Eugene Kolomeetz <klm@virtuozzo.com>
2017-06-13 16:54:30 +0300

    generator: implement stream-like class for reading memory image
    This will allow us to read metadata embedded within patch DSO
    
    Signed-off-by: Eugene Kolomeetz <klm@virtuozzo.com>

--- generator/elffile.py
+++ generator/elffile.py
@@ -7,11 +7,13 @@ from elftools.elf.elffile import ELFFile
 from elftools.elf.elffile import SymbolTableSection
 from elftools.elf.descriptions import describe_p_flags, describe_reloc_type
 from elftools.elf.constants import P_FLAGS, SH_FLAGS
+from elftools.elf.enums import ENUM_SH_TYPE
 
 from consts import *
 from util import memoize
 
 set_const_raw(SH_FLAGS.__dict__)
+set_const_str(ENUM_SH_TYPE)
 
 ElfHeader = namedtuple("ElfHeader", "type machine")
 ElfSym = namedtuple("ElfSym", "num value size type bind vis ndx name")
@@ -130,4 +132,57 @@ class AddressSpace(object):
 		return sec
 
 get_address_space = memoize(WeakKeyDictionary)(AddressSpace)
+
+class MemoryStream(object):
+	def __init__(self, elf):
+		self.elf = elf
+		self._address_space = get_address_space(elf)
+
+	def seek(self, addr):
+		self._addr = addr
+
+	def tell(self):
+		return self._addr
+
+	def _read(self, size):
+		assert size >= 0
+		if not size:
+			return ""
+
+		get_section = self._address_space.get_section
+
+		addr = self._addr
+		next_addr = addr + size
+
+		sec = get_section(addr)
+		if not sec:
+			raise Exception("No section for address {:x}".format(addr))
+
+		next_sec = get_section(next_addr - 1)
+		# Make sure one read() call processes data from one section
+		if sec is not next_sec:
+			next_addr = sec.header.sh_addr + sec.header.sh_size
+			size = next_addr - addr
+
+		if sec.header.sh_type == STR.SHT_NOBITS:
+			data = "\x00" * size
+		else:
+			stream = sec.stream
+			offset = addr - sec.header.sh_addr + sec.header.sh_offset
+			stream.seek(offset)
+			data = stream.read(size)
+			if len(data) != size:
+				raise Exception("Short read: got {}, expected {}".format(
+							len(data), size))
+
+		self._addr = next_addr
+		return data
+
+	def read(self, size, allow_short=False):
+		data = self._read(size)
+		assert len(data) <= size
+		if not allow_short and len(data) < size:
+			raise Exception("Short read: got {}, expected {}".format(
+				len(data), size))
+		return data
 
